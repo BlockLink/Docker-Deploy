f62034ec7e3bac81da3435791d5fddf8c0d079f8
realm520 <realm520@gmail.com>
2018-10-31 15:46:04 +0800

    start with base image quickly

--- install.sh
+++ install.sh
@@ -12,7 +12,13 @@ if [ ! -d "hx" ]; then
     wget https://github.com/HcashOrg/HyperExchange/releases/download/v${HX_WALLET_VERSION}/hx_wallet_linux_v${HX_WALLET_VERSION}.tar.gz
     tar xf hx_wallet_linux_v${HX_WALLET_VERSION}.tar.gz
     mv hx_wallet_linux_v${HX_WALLET_VERSION} hx
-    cp hx/hx_node main_chain/
+    cp third_chain/* hx/
+    cp main_chain/start_main_chain.sh hx/
+    chmod +x hx/*
+    cp main_chain/genesis.json hx/
+    cp -a main_chain/crosschain_midware hx/
+    mkdir -p hx/bitcoin_data hx/hc_data/hcd_data hx/hc_data/hcwallet_data hx/litecoin_data hx/hx_data hx/logs hx/mongo_data
+    cp third_chain/starthc.py hx/hc_data/starthc.py
 fi
 
 docker-compose up
