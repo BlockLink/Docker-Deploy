a8f54320fe562f79a7d4a8e683500fd0652a04a3
realm520 <realm520@gmail.com>
2018-11-01 11:09:06 +0800

    change config of wallet host

--- third_chain/start_third_chain.sh
+++ third_chain/start_third_chain.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-/hx/bitcoind   -testnet -datadir=/hx/bitcoin_data   -rpcuser=a -rpcpassword=b -rpcallowip=192.168.0.0/16 -rpcport=60011 -server  -txindex &
+/hx/bitcoind   -testnet -datadir=/hx/bitcoin_data   -rpcuser=a -rpcpassword=b -rpcallowip=0.0.0.0/0 -rpcport=60011 -server  -txindex &
 /hx/hcd --appdata /hx/hc_data/hcd_data -u a -P b --notls --rpclisten 127.0.0.1:19019 --testnet --whitelist 127.0.0.1/24 --txindex setgenerate true --miningaddr TsVtYhsRxZ1xynRajBqfHP5uvpWFNC5sSHQ &
 python /hx/hc_data/starthc.py &
 sleep 15
 /hx/hcwallet -A /hx/hc_data/hcwallet_data -u a -P b --testnet --pass 12345678 -c 127.0.0.1:19019 --noclienttls --rpclisten :19020 --rpccert=/hx/hc_data/hcwallet_data/rpc.cert  --debuglevel warn &
-/hx/litecoind  -testnet -datadir=/hx/litecoin_data  -rpcuser=a -rpcpassword=b -rpcallowip=192.168.0.0/16 -rpcport=60012 -server  -txindex
+/hx/litecoind  -testnet -datadir=/hx/litecoin_data  -rpcuser=a -rpcpassword=b -rpcallowip=0.0.0.0/0 -rpcport=60012 -server  -txindex
 
