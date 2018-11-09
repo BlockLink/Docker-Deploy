17065cdc73b106009475c9e41d8777bab2cb0a08
zoowii <1992zhouwei@gmail.com>
2018-11-09 17:20:46 +0800

    add third chain nginx conf

--- third_chain/start_third_chain.sh
+++ third_chain/start_third_chain.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+cp /hx/third_chain_nginx_conf /etc/nginx/sites-available/default
+service nginx restart
+
 /hx/bitcoind   -testnet -datadir=/hx/bitcoin_data -testnet  -rpcuser=a -rpcpassword=b -rpcallowip=0.0.0.0/0 -rpcport=60011 -server  -txindex &
 /hx/hcd --appdata /hx/hc_data/hcd_data -u a -P b --notls --rpclisten 127.0.0.1:19019 --testnet --whitelist 127.0.0.1/24 --txindex setgenerate true --miningaddr TsVtYhsRxZ1xynRajBqfHP5uvpWFNC5sSHQ &
 python /hx/hc_data/starthc.py &
