c02cfb3450751993f51db0b8beb0f8635b472bd8
zoowii <1992zhouwei@gmail.com>
2018-11-09 20:19:11 +0800

    fix a bug of hc rpc config

--- third_chain/start_third_chain.sh
+++ third_chain/start_third_chain.sh
@@ -7,6 +7,6 @@ service nginx restart
 /hx/hcd --appdata /hx/hc_data/hcd_data -u a -P b --notls --rpclisten 127.0.0.1:19019 --testnet --whitelist 127.0.0.1/24 --txindex setgenerate true --miningaddr TsVtYhsRxZ1xynRajBqfHP5uvpWFNC5sSHQ &
 python /hx/hc_data/starthc.py &
 sleep 15
-/hx/hcwallet -A /hx/hc_data/hcwallet_data -u a -P b --testnet --pass 12345678 -c 127.0.0.1:19019 --noclienttls --rpclisten :19020 --rpccert=/hx/hc_data/hcwallet_data/rpc.cert  --debuglevel warn &
+/hx/hcwallet -A /hx/hc_data/hcwallet_data -u a -P b --testnet --pass 12345678 -c 127.0.0.1:19019 --noclienttls --rpclisten 127.0.0.1:19020 --noservertls --debuglevel warn &
 /hx/litecoind  -testnet -datadir=/hx/litecoin_data -testnet -rpcuser=a -rpcpassword=b -rpcallowip=0.0.0.0/0 -rpcport=60012 -server  -txindex
 
