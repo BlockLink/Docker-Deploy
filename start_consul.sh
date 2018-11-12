375e968e9e63f79a06b24bab6b9c1423fabee71e
zoowii <1992zhouwei@gmail.com>
2018-11-12 16:10:49 +0800

    add more monitor services to consul

--- start_consul.sh
+++ start_consul.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-./consul -dev -config-dir=./consul.d
+./consul agent -dev -config-dir=./consul.d  >> ./hx/logs/consul_log 2>&1 &
+sleep 5
+curl --request PUT --data @consul_services/collector.json http://127.0.0.1:8500/v1/agent/service/register
+curl --request PUT --data @consul_services/chain_wallets.json http://127.0.0.1:8500/v1/agent/service/register
+curl --request PUT --data @consul_services/geth.json http://127.0.0.1:8500/v1/agent/service/register
+echo "registered consul services"
+
